3d379f96b9d17a3cf668badde37f7e287a4cd09d
abbisk <abbiskpro@gmail.com>
2021-02-06 00:23:48 +0530

    configure calc views

--- myproject/calc/urls.py
+++ myproject/calc/urls.py
@@ -1,10 +1,7 @@
 #base Url: http://127.0.0.1:8000/calc/
 from django.urls import path
 from django.http import HttpResponse
-
-def home_view(required):
-    res = HttpResponse("<h1>Welcome to the calculator homepage</h1>")
-    return res
+from .views import *
 
 urlpatterns = [
     path('', home_view)
--- myproject/calc/views.py
+++ myproject/calc/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_view(required):
+    res = HttpResponse("<h1>Welcome to the calculator homepage</h1>")
+    return res
