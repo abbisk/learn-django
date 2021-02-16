4df83a27812ea26929edb1de1bc02d5137daed46
abbisk <abbiskpro@gmail.com>
2021-02-16 13:31:05 +0530

    django model form for register students

--- myproject/students/views.py
+++ myproject/students/views.py
@@ -1,8 +1,18 @@
+from .forms import *
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(request):
-    res = HttpResponse("This is students home page")
-    return res
+# def home_view(request):
+#     res = HttpResponse("This is students home page")
+#     return res
+
+def view_register_student(request):
+    if request.method=="GET":
+        frmStudent = StudentForm()
+        d1 = {'form':frmStudent}
+        resp = render(request,'students/register_student.html',context=d1)
+        return resp
+    elif request.method=="POST":
+        pass
