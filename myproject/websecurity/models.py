a52808d639758f833587676bb9d0cdfa3c538824
abbisk <abbiskpro@gmail.com>
2021-04-05 01:16:23 +0530

    created user profile model

--- myproject/websecurity/models.py
+++ myproject/websecurity/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    mobileno= models.CharField(max_length=15)
+    address=models.TextField(max_length=500,blank=True,null=True)
+    date_of_birth=models.DateField(blank=True,null=True)
+    pic=models.ImageField(blank=True,null=True,upload_to="user_pic")
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
