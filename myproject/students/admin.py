f62350ff75912217270c42e2724917bc21c140c1
abbisk <abbiskpro@gmail.com>
2021-04-04 23:22:08 +0530

    websecurity pages initialized

--- myproject/students/admin.py
+++ myproject/students/admin.py
@@ -4,7 +4,17 @@ from django.contrib.auth.models import User
 # Register your models here.
 from .models import Student,Course,PaymentDetails,EducationDetails
 
-admin.site.register(Student)
+
+# admin.site.register(Student)
+# Here creating model from model admin
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name','address', 'mobile','emailid']
+
+
+
+
 admin.site.register(Course)
 admin.site.register(PaymentDetails)
 admin.site.register(EducationDetails)
--- myproject/websecurity/urls.py
+++ myproject/websecurity/urls.py
@@ -1,6 +1,5 @@
 #base Url: http://127.0.0.1:8000/security/
 from django.urls import path
-from django.http import HttpResponse
 from .views import *
 
 urlpatterns = [
